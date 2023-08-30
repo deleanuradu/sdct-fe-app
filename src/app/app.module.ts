5f9e6fecbd2990b9161832659954f79e8c17bf3a
Deleanu Radu <radu_deleanu@outlook.com>
2023-08-30 17:03:55 +0300

    added radar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,30 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 // used to create fake backend
-import { fakeBackendProvider } from './_helpers';
+import { ErrorInterceptor, fakeBackendProvider, JwtInterceptor } from './_helpers';
 
 import { AppRoutingModule } from './app-routing.module';
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
 import { ResultsComponent } from '@app/results/results.component';
+import { NgChartsModule } from "ng2-charts";
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        AlertComponent,
-        HomeComponent,
-        ResultsComponent
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule, NgChartsModule.forRoot()],
+  declarations: [AppComponent, AlertComponent, HomeComponent, ResultsComponent],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+  },
 
-        // provider used to create fake backend
-        fakeBackendProvider
-    ],
-    bootstrap: [AppComponent]
+    // provider used to create fake backend
+    fakeBackendProvider],
+  bootstrap: [AppComponent]
 })
-export class AppModule { };
+export class AppModule {
+}
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SurveyService } from "@app/survey/survey.service";
 import { reportUnhandledError } from "rxjs/internal/util/reportUnhandledError";
+import { ChartConfiguration, ChartData, ChartType } from "chart.js";
 
 @Component({
   selector: 'app-results', templateUrl: './results.component.html', styleUrls: ['./results.component.less']
@@ -8,28 +9,56 @@ import { reportUnhandledError } from "rxjs/internal/util/reportUnhandledError";
 export class ResultsComponent implements OnInit {
 
   results = {
-    sysAnalyst: 0, softwareDev: 0, webMultimedia: 0, appProgrammer: 0,
+    sysAnalyst: 0,
+    softwareDev: 0,
+    webMultimedia: 0,
+    appProgrammer: 0,
   }
+
   sysAnalystScore: number = 0;
   softwareDevScore: number = 0;
   webMultimediaScore: number = 0;
   appProgrammerScore: number = 0;
+
   min1 = this.surveyService.sysAnalystMap.length;
   min2 = this.surveyService.softwareDevMap.length;
   min3 = this.surveyService.webAndMultimediaMap.length;
   min4 = this.surveyService.appProgrammerMap.length;
+
   max1 = this.min1 * 5;
   max2 = this.min2 * 5;
   max3 = this.min3 * 5;
   max4 = this.min4 * 5;
+
+  public radarChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public radarChartLabels: string[] = [
+    'System Analyst',
+    'Software Developer',
+    'Web and Multimedia Developer',
+    'Application Developer'
+  ];
+  public radarChartType: ChartType = 'radar';
+  public radarChartData: ChartData<'radar'> | undefined;
   protected readonly reportUnhandledError = reportUnhandledError;
 
   constructor(private surveyService: SurveyService) {
     this.getAvailableScores();
     this.computeResults();
+    this.initCharts();
   }
 
   ngOnInit(): void {
+  }
+
+  public initCharts(): void {
+    this.radarChartData = {
+      labels: this.radarChartLabels, datasets: [{
+        data: [this.results.sysAnalyst, this.results.softwareDev, this.results.webMultimedia, this.results.appProgrammer,],
+        label: 'Your results'
+      },],
+    };
   }
 
   private getAvailableScores(): void {
@@ -48,9 +77,14 @@ export class ResultsComponent implements OnInit {
 
   // expressed in percentages
   private computeResults(): void {
-    this.results.sysAnalyst = +((this.sysAnalystScore - this.min1) / (this.max1 - this.min1) * 100).toFixed(2);
-    this.results.softwareDev = +((this.softwareDevScore - this.min2) / (this.max2 - this.min2) * 100).toFixed(2);
-    this.results.webMultimedia = +((this.webMultimediaScore - this.min3) / (this.max3 - this.min3) * 100).toFixed(2);
-    this.results.appProgrammer = +((this.appProgrammerScore - this.min4) / (this.max4 -  - this.min4) * 100).toFixed(2);
+    this.results.sysAnalyst =
+      +((this.sysAnalystScore - this.min1) / (this.max1 - this.min1) * 100).toFixed(2);
+    this.results.softwareDev =
+      +((this.softwareDevScore - this.min2) / (this.max2 - this.min2) * 100).toFixed(2);
+    this.results.webMultimedia =
+      +((this.webMultimediaScore - this.min3) / (this.max3 - this.min3) * 100).toFixed(2);
+    this.results.appProgrammer =
+      +((this.appProgrammerScore - this.min4) / (this.max4 - -this.min4) * 100).toFixed(2);
   }
+
 }
